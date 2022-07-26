f45970f98227c707956c514e084483fb074aecfb
lee-410 <58701102+lee-410@users.noreply.github.com>
2022-07-26 14:32:27 +0900

    자바를 자바라

---
+++ helloworld/src/Helloworld.java
@@ -0,0 +1,9 @@
+
+public class Helloworld { //Hello world class 안에 main method를 생성했음. 초기 설정
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world! im taeran");
+	}
+
+}
