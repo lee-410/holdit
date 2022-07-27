37011c98dfbe50671e51550cae96d6cba964ea6f
lee-410 <58701102+lee-410@users.noreply.github.com>
2022-07-27 23:37:50 +0900

    helloworld2

--- helloworld/src/helloworld/Helloworld2.java
+++ helloworld/src/helloworld/Helloworld2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Helloworld { 
+public class Helloworld2 { 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num1, num2;
