e000ef503a2d6c442fc424f0473d2cd3b61a949a
lee-410 <58701102+lee-410@users.noreply.github.com>
2022-07-27 23:54:59 +0900

    Helloworld2

--- helloworld/src/Helloworld2.java
+++ helloworld/src/Helloworld2.java
@@ -1,16 +1,9 @@
-import java.util.Scanner;
 
-public class Helloworld2 { 
+public class Helloworld2 {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num1, num2;
-		Scanner sc = new Scanner(System.in);
-		num1 = sc.nextInt();
-		num2 = sc.nextInt();
-		if (num1>0 && num2<10) {
-			System.out.println(num1+num2);
-		}else {
-			System.out.println("input number must be 0 to 10");
-		}
+
 	}
+
 }
